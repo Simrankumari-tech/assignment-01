3dd55489d2e3ea1ea65b17a0cb6cadd9e2812f62
Simran kumari <simrankumari.nea10b@gmail.com>
2024-07-15 23:58:56 +0530

    Update manmohan_loves_pattern02.cpp

--- manmohan_loves_pattern02.cpp
+++ manmohan_loves_pattern02.cpp
@@ -6,19 +6,23 @@
 
 
 
-
 #include <iostream>
 using namespace std;
 int main(){
     int n;
     cin >> n;
-    cout<< "1" << endl;
-    for(int i=1; i<n; i++){
-        cout << i;
-        for(int j =1; j<i; j++){
-            cout << "0" ;
+    for(int i=1 ;i<=n; i++){
+        for(int j=1; j<=i; j++){
+            if(i==1){
+                cout << i;
+            }
+            else if(j==1 || j== i){
+                cout << i-1;
+            }
+            else{
+                cout << "0" ;
+            }
         }
-        cout << i;
         cout << endl;
     }
         
